a9e3bc32a3db5f9a685b6ca24c2d0a9eca7cd727
B <konta1231@gmail.com>
2025-05-04 09:58:45 +0530

    Added dashboard button for checking db page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -29,6 +30,12 @@ export default function Page() {
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
+          <Link 
+            href="/dashboard"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          > Checkout Dashaboard
+          </Link>
+
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
