5eeb2a7abe0b3ea5163a96f91a1f89cec27cce89
B <konta1231@gmail.com>
2025-05-04 09:59:12 +0530

    fix1

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Page() {
           <Link 
             href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          > Checkout Dashaboard
+          > Checkout Dashboard
           </Link>
 
         </div>
